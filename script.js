0a2101d8e49afeba2568dfafd4dab3ed5e8f2520
Dewa KD <86773325+Dewakd@users.noreply.github.com>
2024-05-15 13:48:54 +0800

    api testing

---
+++ script.js
@@ -0,0 +1,46 @@
+const apiKey = '215985a1-0f74-5e25-e3e1-fa52a14f';
+const apiUrl = `https://api.goapi.io/regional/provinsi?api_key=${apiKey}`
+
+axios.get(apiUrl)
+    .then(response => {
+        const provinsi = response.data.data;
+        console.log(provinsi);
+
+        const list_provinsi = document.getElementById('list_provinsi');
+
+        provinsi.forEach(city => {
+            const listItem = document.createElement('li');
+            const linkItem = document.createElement('a');
+            linkItem.href = '#';
+            linkItem.textContent = city.name;
+            listItem.addEventListener('click', () => {
+                axios.get(`https://api.goapi.io/regional/kota?provinsi_id=${city.id}&api_key=${apiKey}`)
+                .then(response => {
+                    const kab_provinsi = response.data.data;
+                    console.log(kab_provinsi)
+                    const list_kab = document.getElementById('list_kabupaten');
+                    
+                    kab_provinsi.forEach(kabupaten => {
+                        const listItem = document.createElement('li');
+                        listItem.textContent = kabupaten.name;
+                        list_kab.appendChild(listItem);
+                    })
+
+                })
+            
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+
+            });
+            list_provinsi.appendChild(listItem);
+            listItem.appendChild(linkItem);
+        });
+            
+
+
+    })
+
+    .catch(error => {
+        console.error('Error:', error);
+    });
